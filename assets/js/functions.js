4ee55b23277493c862d991853e52142475759fde
Nassim Koceir <nassim.koceir@gmail.com>
2021-10-07 15:18:34 +0200

    Responsive menu fix

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,4 @@
+/* OPEN OR CLOSE THE MENU */
 function openNav() {
   document.getElementById("left-panel").style.left = "0";
   document.getElementById("left-panel").style.overflowX = "visible";
@@ -6,4 +7,16 @@ function openNav() {
 function closeNav() {
   document.getElementById("left-panel").style.left = "-450px";
   document.getElementById("left-panel").style.overflowX = "hidden";
+}
+
+
+/* DETECT WINDOW RESIZING WHEN TEST THE RESPONSIVENESS ;) */
+window.addEventListener('resize', windowSize);
+
+function windowSize() {
+  if(window.innerWidth >= 992){
+	  openNav();
+  } else {
+	  closeNav();
+  }
 }
