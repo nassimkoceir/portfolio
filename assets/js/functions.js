baab991ad516c6e201525f5a35a73dcd6154de08
Nassim Koceir <nassim.koceir@gmail.com>
2021-10-07 17:15:34 +0200

    Fix nav button displayed under the menu when it on phone

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,11 +2,13 @@
 function openNav() {
   document.getElementById("left-panel").style.left = "0";
   document.getElementById("left-panel").style.overflowX = "visible";
+  document.getElementById("top-navigation").style.opacity = "0";
 }
 
 function closeNav() {
   document.getElementById("left-panel").style.left = "-450px";
   document.getElementById("left-panel").style.overflowX = "hidden";
+  document.getElementById("top-navigation").style.opacity = "0";
 }
 
 
